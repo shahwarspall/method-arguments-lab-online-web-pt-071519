d99e5b0096e9d29afef31530b4316b10d9ff4d40
Shahwar Spall <shevy.spall@gmail.com>
2019-07-15 22:58:14 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,7 +2,7 @@ def introduction (name)
   puts "Hi, my name is #{name}."
 end
 introduction
-end
+
 
 def introduction (name,language)
   puts "Hi, my name is #{name} and I am learning to problem in #{language}."
